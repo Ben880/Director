3b44ff9a7c8700b6344b60836c229bed795e40c6
Ben880 <Ben880@users.noreply.github.com>
2019-10-15 16:04:38 -0400

    Point Script Update
    +EnableRandom script
    +Spawn script to spawn specific item
    -old code from D-SpawnRandom

---
+++ Assets/Director/Scripts/Points/D_EnableRandom.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class D_EnableRandom : D_PointObject
+{
+    public bool canEnableMultipleObjects = false;
+    public List<GameObject> enableObjects = new List<GameObject>();
+
+    void Start()
+    {
+        foreach (GameObject obj in enableObjects)
+        {
+            obj.SetActive(false);
+        }
+    }
+
+    public override void trigger()
+    {
+        int random = Random.Range(0, enableObjects.Count - 1);
+        enableObjects[random].SetActive(true);
+        if (canEnableMultipleObjects)
+        {
+            enableObjects.RemoveAt(random);
+            spawnable = enableObjects.Count > 0;
+        }
+        else
+        {
+            spawnable = false;
+        }
+
+    }
+
+
+}
---
+++ Assets/Director/Scripts/Points/D_Spawn.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class D_Spawn : D_PointObject
+{
+
+    public bool canSpawnMultipleTimes = false;
+    public GameObject spawnObjects = new GameObject();
+
+
+    public override void trigger()
+    {
+        Instantiate(spawnObjects, gameObject.transform.position, gameObject.transform.rotation);
+        if (!canSpawnMultipleTimes)
+            spawnable = false;
+    }
+    
+}
