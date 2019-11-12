33993fae79e7f7bbb5ab6360cb15828406b9bd5a
Ben880 <Ben880@users.noreply.github.com>
2019-11-12 18:47:39 -0500

    Velocity Tracking
    added velocity data collection to director
    + D_UpdateVelocity
    + D_CircularArray

---
+++ Assets/Director/Scripts/Inputs/D_UpdateVelocity.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class D_UpdateVelocity : D_InputObject
+{
+    public float updateInterval = 0.1f;
+    public int[] dataIntervals = new int[0];
+
+    private int updatesPerSecond;
+    private D_CircularArray<Vector3> positions;
+    private int largest = 0;
+    private float counter = 0;
+    
+    //private List<Tuple<float, Vector3>> positions = new List<Tuple<float, Vector3>>();
+    //positions.Add(new Tuple<float, Vector3>(director.getData().getFloat("Total Time").value, director.getPlayer().transform.position));
+    //private Queue<Vector3> positions;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        base.Start();
+        // if there is no collection interval create the default array
+        if (dataIntervals.Length == 0)
+            dataIntervals = new int[1] {1};
+        // for each data interval create a key for the director data and find the largest interval
+        foreach (var val in dataIntervals)
+        {
+            director.getData().getFloat("Velocity " + val).value = 0;
+            if (val > largest)
+                largest = val;
+        }
+        // with the largest interval create a new circular array;
+        updatesPerSecond = Mathf.RoundToInt(1 / updateInterval);
+        positions = new D_CircularArray<Vector3>(largest*updatesPerSecond);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        counter += Time.deltaTime;
+        if (counter >= updateInterval)
+        {
+            counter = 0;
+            positions.Add(director.getPlayer().transform.position);
+            foreach (var val in dataIntervals)
+            {
+                int refrence = (val * updatesPerSecond * -1) - 1;
+                if (positions.testValid(refrence))
+                {
+                    float totalDistance = 0;
+                    for (int i = refrence; i < 0; i++)
+                    {
+                        totalDistance += Vector3.Distance(positions.Get(0), positions.Get(i));
+                    }
+
+                    director.getData().getFloat("Velocity " + val).value = totalDistance / val;
+                }
+            }
+   
+        }
+    }
+
+
+}
---
+++ Assets/Director/Scripts/Tools/D_CircularArray.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class D_CircularArray<T>
+{
+
+    private T[] array;
+    private int position = 0;
+    private int len = 0;
+
+    public D_CircularArray(int length)
+    {
+        array  = new T[length];
+        len = length;
+    }
+    
+
+    public void Add(T input)
+    {
+        position++;
+        if (position > len-1)
+            position = 0;
+        array[position] = input;
+    }
+
+    public T Get(int i)
+    {
+        return array[convert(i)];
+    }
+
+    public bool testValid(int i)
+    {
+        return array[convert(i)] != null;
+        
+    }
+
+    private int convert(int i)
+    {
+        int offset = (i%len) + position;
+        if (offset < 0)
+        {
+            return len + offset;
+        }
+        else if (i > len-1)
+        {
+            return offset % len;
+        }
+        return offset;
+    }
+
+    public int length()
+    {
+        return len;
+    }
+}
