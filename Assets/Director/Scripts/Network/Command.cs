35132252693842b5143ff2a778afeb7af5247ec2
Ben880 <Ben880@users.noreply.github.com>
2020-02-20 15:25:24 -0500

    Revert "MoveFolder"
    This reverts commit a89fef1c892a07dc9eafda2a9e2fbee029fe4d4e.

---
+++ Assets/Director/Scripts/Network/Command.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Command
+{
+    private string commandName = "";
+    private bool enabled = false;
+    private List<NotifyObject> notifyObjects = new List<NotifyObject>();
+
+    // constructor that sets all needed data
+    public Command(string name, bool enabled)
+    {
+        commandName = name;
+        this.enabled = enabled;
+    }
+    
+    // get the name of the command
+    public string getName()
+    {
+        return commandName;
+    }
+    public bool isEnabled()
+    {
+        return enabled;
+    }
+    public void setEnabled(bool enabled)
+    {
+        this.enabled = enabled;
+        
+    }
+
+    public void registerNotifyObject(NotifyObject notifyObject)
+    {
+        notifyObjects.Add(notifyObject);
+    }
+
+    public void execute()
+    {
+        foreach (NotifyObject notifyObj in notifyObjects)
+        {
+            notifyObj.notify();
+        }
+    }
+    
+}
---
+++ Assets/Director/Scripts/Network/NetworkSettings.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetworkSettings
+{
+    private string id = "";
+    private bool publicSession = true;
+    private Communication communication;
+
+    public NetworkSettings(Communication communication)
+    {
+        this.communication = communication;
+    }
+
+    public string ID
+    {
+        get { return id; }
+        set
+        {
+            id = value;
+            notifyServerOfChange("ID", value);
+        }
+    }
+
+    public bool PublicSession
+    {
+        get { return publicSession; }
+        set
+        {
+            publicSession = value;
+            notifyServerOfChange("PublicSession", value);
+        }
+    }
+
+    private void notifyServerOfChange(string key, dynamic value)
+    {
+        PacketObject packet = new PacketObject();
+        packet.setDestination("NetworkSettings");
+        packet.setCommand("ChangeSetting");
+        packet.addNode(new PacketNode(key, value));
+        communication.sendToServer(packet);
+    }
+}
