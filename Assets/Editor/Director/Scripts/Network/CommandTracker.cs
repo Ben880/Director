a89fef1c892a07dc9eafda2a9e2fbee029fe4d4e
Ben880 <Ben880@users.noreply.github.com>
2020-02-20 14:01:20 -0500

    MoveFolder
    Moved scripts under an editor folder

---
+++ Assets/Editor/Director/Scripts/Network/CommandTracker.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using UnityEngine;
+
+public class CommandTracker: MonoBehaviour 
+{
+    private Dictionary<string, Command> commands = new Dictionary<string, Command>();
+    private Communication communication;
+    void Awake()
+    {
+        communication = GetComponent<Communication>();
+    }
+    
+    public void registerCommand(Command command)
+    {
+        commands.Add(command.getName(), command);
+        PacketObject po = new PacketObject();
+        po.setDestination("ServerCommandTracker");
+        po.setCommand("RegisterCommand");
+        po.addNode(new PacketNode("CommandName", command.getName()));
+        po.addNode(new PacketNode("Enabled", command.isEnabled().ToString()));
+    }
+    
+    //will be depreciated after protobuff
+    public void recievedCommand(PacketObject po)
+    {
+        Command commandToExecute = commands[po.getCommand()];
+        if (commandToExecute == null)
+            Debug.LogError("Command to execute is null");
+        else if (!commandToExecute.isEnabled())
+            Debug.LogError("Command to execute is disabled");
+        else
+        {
+            commands[po.getCommand()].execute();
+        }
+    }
+
+    public void executeCommand(string nameOfCommandToExecute)
+    {
+        Command commandToExecute = commands[nameOfCommandToExecute];
+        if (commandToExecute == null)
+            Debug.LogError("Command to execute is null");
+        else if (!commandToExecute.isEnabled())
+            Debug.LogError("Command to execute is disabled");
+        else
+        {
+            commandToExecute.execute();
+        }
+    }
+    // ==================================================
+    // ===methods for restricted access of commands======
+    // ==================================================
+    private Command getCommand(string key)
+    {
+        if (commands.ContainsKey(key))
+            return commands[key];
+        else
+            Debug.LogError($"Key ({key}) does not exist", this);
+        throw new Exception("No such key");
+    }
+
+    public void setCommandEnabled(string key, bool value)
+    {
+        getCommand(key).setEnabled(value);
+        PacketObject packet = new PacketObject();
+        packet.setDestination("CommandTracker");
+        packet.setCommand("SetCommandEnabled");
+        packet.addNode(new PacketNode("Key", key));
+        packet.addNode(new PacketNode("Value", value.ToString()));
+        communication.sendToServer(packet);
+    }
+
+    public void addNotifyObject(string key, NotifyObject notifyObj)
+    {
+        getCommand(key).registerNotifyObject(notifyObj);
+    }
+
+    public bool commandExists(string nameOfCommand)
+    {
+        return commands.ContainsKey(nameOfCommand);
+    }
+    
+    
+}
---
+++ Assets/Editor/Director/Scripts/Network/MetricCompiler.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+[RequireComponent(typeof(Communication))]
+public class MetricCompiler : MonoBehaviour
+{
+    // ======================================================combination of send less and compute less
+    //access to the data collection through D_DirectorObjects (Specifically .getData() method)
+    private D_DirectorObjects director;
+    //access to server communications
+    private Communication communication;
+
+    [SerializeField] 
+    private float sendEveryMS = 100;
+    private float counter = 0;
+
+
+    void Awake()
+    {
+        director = new D_DirectorObjects();
+        communication = GetComponent<Communication>();
+    }
+
+    void Update()
+    {
+        counter += Time.deltaTime;
+        if (counter >= sendEveryMS/1000)
+        {
+            sendData();
+            counter = 0;
+        }
+    }
+
+    private void sendData()
+    {
+        
+        PacketObject packet = new PacketObject();
+        packet.setDestination("MetricCompiler");
+        packet.setCommand("UpdateData");
+        foreach (var floatObj in director.getData().getFloatList())
+        {
+            packet.addNode(new PacketNode(floatObj.key, floatObj.value.ToString()));
+        }
+        //send data string to the server
+        communication.sendToServer(packet);
+        Debug.Log("sending packet to communication moduel");
+    }
+}
